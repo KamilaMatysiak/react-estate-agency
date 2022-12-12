add9dee2a60d9d4b90775965194ff49070519cbf
Tomek Świątek <tomek.swarzedz@gmail.com>
2022-12-12 12:55:21 +0100

    Sidebar layout

--- client/src/components/adminPanel/Sidebar.jsx
+++ client/src/components/adminPanel/Sidebar.jsx
@@ -1,9 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import theme from '../Theme'
 import { Box, ThemeProvider, Typography, Button } from '@mui/material'
 import { NavLink, useNavigate, useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import decode from 'jwt-decode'
+import './Sidebar.css'
+
 
 const Sidebar = () => {
   const dispatch = useDispatch();
@@ -12,7 +14,7 @@ const Sidebar = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
 
   const logout = () => {
-    dispatch({type: 'LOGOUT'});
+    dispatch({ type: 'LOGOUT' });
     //navigate('/');
     window.location.reload(false);
     setUser(null);
@@ -20,26 +22,35 @@ const Sidebar = () => {
 
 
   useEffect(() => {
-      const token = user?.token;
+    const token = user?.token;
 
-      if(token) {
-          const decodedToken = decode(token);
-          if(decodedToken.exp * 1000 < new Date().getTime()) 
-              logout();
-      }
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.exp * 1000 < new Date().getTime())
+        logout();
+    }
 
-      setUser(JSON.parse(localStorage.getItem('profile')));
+    setUser(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{backgroundColor: "primary.main", minHeigth: '100%', width:250, display: 'flex', flexDirection:'column'}}>
-        <NavLink style={{color: "#fff", textDecoration: 'none', padding: '16px 24px'}} to="/dashboard"><Typography>Home</Typography></NavLink>
-        <NavLink style={{color: "#fff", textDecoration: 'none', padding: '16px 24px'}} to="/dashboard">Employees</NavLink>
-        <NavLink style={{color: "#fff", textDecoration: 'none', padding: '16px 24px'}} to="/dashboard">Estates</NavLink>
-        <NavLink style={{color: "#fff", textDecoration: 'none', padding: '16px 24px'}} to="/dashboard">Offers</NavLink>
-        <NavLink style={{color: "#fff", textDecoration: 'none', padding: '16px 24px'}} to="/dashboard">Tenants</NavLink>
-        <Button color="secondary" onClick={() => {logout()}}>Log Out</Button>
+      <Box sx={{ backgroundColor: "primary.main", minHeigth: '100%', width: 250, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div className="admin-panel">
+          <Typography variant="lg">Admin Panel</Typography>
+        </div>
+        <Typography variant="sm">
+          <div className="menu-list">
+            <NavLink style={{ color: "#fff", textDecoration: 'none', padding: '16px 24px' }} to="/dashboard">Home</NavLink>
+            <NavLink style={{ color: "#fff", textDecoration: 'none', padding: '16px 24px' }} to="/dashboard">Employees</NavLink>
+            <NavLink style={{ color: "#fff", textDecoration: 'none', padding: '16px 24px' }} to="/dashboard">Estates</NavLink>
+            <NavLink style={{ color: "#fff", textDecoration: 'none', padding: '16px 24px' }} to="/dashboard">Offers</NavLink>
+            <NavLink style={{ color: "#fff", textDecoration: 'none', padding: '16px 24px' }} to="/dashboard">Tenants</NavLink>
+          </div>
+        </Typography>
+        <div className="button">
+          <Button variant="text.white" onClick={() => { logout() }}>Logout</Button>
+        </div>
       </Box>
     </ThemeProvider>
   )
